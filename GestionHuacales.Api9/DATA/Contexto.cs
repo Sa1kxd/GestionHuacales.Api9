3c78bf969898a2a3e65711034930f1aebf7c0914
Sa1kxd <samildelacruz1@gmail.com>
2025-10-24 12:02:06 -0400

    Contexto creado

---
+++ GestionHuacales.Api9/DATA/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GestionHuacales.Api9.Models;
+namespace GestionHuacales.Api9.DATA;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+    {
+    }
+    public DbSet<EntradasHuacales> EntradasHuacales { get; set; }
+    public DbSet<TiposHuacales> TiposHuacales { get; set; }
+    public DbSet<EntradasHuacalesDetalle> EntradasHuacalesDetalle { get; set; }
+}
