ffb75fa3aa1c83f5262103471151d0ddb47e51e4
Sa1kxd <samildelacruz1@gmail.com>
2025-10-24 12:04:09 -0400

    OnmodelCreating agregado

--- GestionHuacales.Api9/DATA/Contexto.cs
+++ GestionHuacales.Api9/DATA/Contexto.cs
@@ -10,4 +10,28 @@ public class Contexto : DbContext
     public DbSet<EntradasHuacales> EntradasHuacales { get; set; }
     public DbSet<TiposHuacales> TiposHuacales { get; set; }
     public DbSet<EntradasHuacalesDetalle> EntradasHuacalesDetalle { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TiposHuacales>().HasData(
+            new List<TiposHuacales>
+            {
+                new()
+                {
+                    TipoId = 1,
+                    Descripcion = "Verde",
+                    Existencia = 0
+                },
+                new()
+                {
+                    TipoId = 2,
+                    Descripcion = "Rojo",
+                    Existencia = 0
+                }
+
+            }
+        );
+        base.OnModelCreating(modelBuilder);
+    }
 }
